a7aeb6bf43ab8c2ef37d49ec651a18d3b7f7368d
harshadev-7 <2024cse.r102@svce.edu.in>
2025-10-19 18:46:22 +0530

    First commit

---
+++ Inheritance/SingleIneritance/Access Modifiers/TestAccount.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+class BankAccount
+{
+	protected double balance;
+	public BankAccount(double balance)
+	{
+		this.balance = balance;
+	} 
+	public void deposit(double deposit){
+	if(deposit>0)
+	{
+		System.out.println("Total amount"+(balance+=deposit));
+	}
+	else {
+		System.out.println("invalid");
+		}
+	}
+	public void withdraw(double withdraw)
+	{
+		if(withdraw > 0  && balance>=withdraw)
+		{
+			System.out.println("withdraw amount is"+(balance-=withdraw));
+		}
+		else
+		{
+			System.out.println("invalid to withdraw");
+		}
+	}
+	void currentbalance()
+	{
+		System.out.println("current balance"+balance);
+	}
+}
+class Saveaccount extends BankAccount
+{
+	public Saveaccount(double balance)
+	{
+		super(balance);
+	}
+	public void interest(double rate)
+	{
+		double interest = balance * rate/100;
+		System.out.println("rate of interest is"+interest);
+	}
+
+	
+}
+public class TestAccount
+{
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		double n = 4000000;
+		Saveaccount save = new Saveaccount(n);
+		Boolean running = true;
+		System.out.println("enter your choice");
+		System.out.println("1. balance");
+		System.out.println("2. deposit");
+		System.out.println("3.withdraw");
+		System.out.println("4. intrest");
+		System.out.println("0. exit");
+		while(running){
+		int choice = scan.nextInt();
+		System.out.println("enter your choice");
+		System.out.println("1. balance");
+		System.out.println("2. deposit");
+		System.out.println("3.withdraw");
+		System.out.println("4. intrest");
+		System.out.println("0. exit");
+		switch(choice)
+		{
+			case 1:
+			{
+				save.currentbalance();
+				break;
+			}
+			case 2:
+			{
+				System.out.println("deposit the amount to the bank");
+				n = scan.nextDouble();
+				save.deposit(n);
+				break;
+			}
+			case 3:
+			{
+				System.out.println("enter the amount you want to with draw");
+				n = scan.nextDouble();	
+				save.withdraw(n);
+				break;
+			}
+			case 4:
+			{
+				System.out.println("enter the interest rate % :");
+				n = scan.nextDouble();
+				save.interest(n);
+				break;
+			}
+			case 0:
+			{
+				running = false;
+				System.out.println("Thank you for banking with as");
+				break;	
+			}
+			default:
+			System.out.println("invalid option");
+	
+		}
+				}
+		scan.close();
+		
+
+	}
+}
